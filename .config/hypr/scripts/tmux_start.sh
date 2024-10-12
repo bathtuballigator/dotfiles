6c8d101dcd5a49a9e1666139f6b1fa83f802c7a9
bathtuballigator <rosetimlin22@gmail.com>
2024-10-12 01:59:21 -0400

    some updates to tmux start script

--- .config/hypr/scripts/tmux_start.sh
+++ .config/hypr/scripts/tmux_start.sh
@@ -1,27 +1,28 @@
 #!/bin/zsh
 
-name=$1
-
 function start_tmux() {
     tmux new -s $1 -c $PWD -n editor -d
-    tmux new-window -a -n runner -t $1":editor" -c $PWD
-    tmux new-window -a -n terminal -t $1":runner" -c $PWD
+    tmux new-window -a -n "file manager" -t $1":editor" -c $PWD nvim
+    tmux split-window -t $1":file manager" -v -c $PWD yazi
+    tmux split-window -t $1":file manager" -h -c $PWD htop
+    tmux new-window -a -n terminal -t $1":file manager" -c $PWD
     tmux send-keys -t editor "nvim" Enter
     tmux select-window -t $1":editor"
     tmux attach -t $1
 }
 
 if [[ $# -eq 0 ]] || [[ $# -gt 1 ]]; then
-    echo "please provide 1 and only 1 argument"
-    exit
+    name="main"
+else
+    name=$1
 fi
 
 tmux ls 2>/dev/null | rg $1 >/dev/null 2>/dev/null
 
 if [[ $? -eq 0 ]]; then
-    tmux attach -t $1
+    tmux attach -t $name
 else
-    start_tmux $1
+    start_tmux $name
 fi
 
 
